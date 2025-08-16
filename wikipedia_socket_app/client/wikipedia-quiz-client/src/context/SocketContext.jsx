99b7bfc83738d5f3ef06596a809bcded3b2f5207
Shadrach Jensen <jensen.shadrach@gmail.com>
2025-08-15 23:22:15 -0600

    Adjusted config files to allow external connections
    Changed npm run dev to host on the local ip and allow devices on the same network to connect via the ipv4 address of the hosting system

--- wikipedia_socket_app/client/wikipedia-quiz-client/src/context/SocketContext.jsx
+++ wikipedia_socket_app/client/wikipedia-quiz-client/src/context/SocketContext.jsx
@@ -1,15 +1,23 @@
 import React, { createContext, useContext} from 'react';
 import { io } from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
 const SocketContext = createContext();
-
-
 
 export const SocketContextProvider = ({ children }) => {
 
     let userId = localStorage.getItem("userId");
     if (!userId) {
-        userId = crypto.randomUUID();
+
+        //only call crypto if it is recognized by the browser as a function
+        if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+            //generate a user id using the crypto call 
+            userId = crypto.randomUUID();
+        } else {
+            //if the crypto call isn't supported by the browser generate an id using the uuid library
+            userId = uuidv4();
+        }
+        
         localStorage.setItem("userId", userId);
     }
 
